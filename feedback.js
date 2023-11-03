ae1ff907d8e0215dbbff6a35d75a3a90aeb6cd46
Frasat ALi Gujjar <127954439+FrasatAliGujjar@users.noreply.github.com>
2023-11-03 08:31:45 -0700

    v1
    v1

---
+++ feedback.js
@@ -0,0 +1,33 @@
+function fawhatsapp() {
+
+    let url = "http://wa.me/923419385624?text="
+
+        + "*Asslam-o-Alaikum !*" + "%0a"
+        + " " + "%0a"
+        + "*Sir ! Frasat Ali Gujjar*" + "%0a"
+        + " " + "%0a"
+        + "*This is my FeedBack*" + "%0a"
+        + "---------------------" + "%0a"
+        + " " + "%0a"
+        + document.getElementById('feedback').value + "%0a"
+        + " " + "%0a"
+        + "---------------------" + "%0a"
+        + " " + "%0a"
+
+    window.open(url, "_blank");
+
+}
+
+
+function contact() {
+
+    let url = "http://wa.me/923017190803?text="
+
+        + "*Asslam-o-Alaikum !*" + "%0a"
+        + "---------------------" + "%0a"
+
+
+    window.open(url, "_blank");
+
+}
+
